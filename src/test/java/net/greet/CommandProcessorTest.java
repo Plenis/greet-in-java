a80b0cd396e937faa62439f2629900ea23e98c94
Sino Plenis <plenissino@gmail.com>
2020-08-19 14:39:05 +0200

    added unit test for jdbc

--- src/test/java/net/greet/CommandProcessorTest.java
+++ src/test/java/net/greet/CommandProcessorTest.java
@@ -1,10 +1,19 @@
 package net.greet;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandProcessorTest {
+    @BeforeEach
+    void clearDB(){
+        JDBC jdbc = new JDBC();
+        CommandExtractor command = new CommandExtractor("clear");
+        CommandProcessor commandProcessor = new CommandProcessor(jdbc);
+
+        commandProcessor.processCommands(command);
+    }
 
     @Test
     void shouldGreetUser(){
@@ -15,6 +24,17 @@ class CommandProcessorTest {
         assertEquals("Molo, Sino", commandProcessor.processCommands(command));
     }
 
+    @Test
+    void shouldGetUserCounter(){
+        Greeting greet = new JDBC();
+        new CommandExtractor("greet sino");
+        new CommandExtractor("greet sino");
+
+        CommandExtractor command = new CommandExtractor("greeted sino");
+        CommandProcessor commandProcessor = new CommandProcessor(greet);
+
+        assertEquals("{}", commandProcessor.processCommands(command));
+    }
 
     @Test
     void shouldClearUser(){
--- src/test/java/net/greet/JDBCTest.java
+++ src/test/java/net/greet/JDBCTest.java
@@ -1,37 +1,47 @@
 package net.greet;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JDBCTest {
+class JDBCTest{
 
-//    @Test
-//    void shouldGreetUser(){
-////        JDBC greet = new JDBC();
-////        assertEquals("Hello, Sino", greet.greeter("Sino", "English"));
-//    }
-//
-//    @Test
-//    void shouldShowGreetedUsers(){
-//        JDBC greet = new JDBC();
-//        greet.greeter("Sino", "IsiXhosa");
-//        greet.greeter("Lala", "English");
-//
-//        assertEquals("{Sino=1, Lala=1}", greet.greeted());
-//    }
-//
-//    @Test
-//    void shouldShowGreetedUser(){
-//        JDBC greet = new JDBC();
-//        greet.greeter("Sino", "English");
-//        greet.greeter("Sino", "English");
-//        greet.greeter("Aya", "Afrikaans");
-//        greet.greeter("Aya", "IsiXhosa");
-//        greet.greeter("Lala", "IsiXhosa");
-//        greet.greeter("Aya", "English");
-//
-//        assertEquals("Aya has been greeted 6 time(s)", greet.greetedUser("Aya"));
-//    }
+    @BeforeEach
+    void clearDB(){
+        JDBC jdbc = new JDBC();
+        CommandExtractor command = new CommandExtractor("clear");
+        CommandProcessor commandProcessor = new CommandProcessor(jdbc);
+
+        commandProcessor.processCommands(command);
+    }
+
+    @Test
+    void shouldGreetUser(){
+        JDBC greet = new JDBC();
+        assertEquals("Hello, Sino", greet.greeter("Sino", "English"));
+    }
+
+    @Test
+    void shouldShowGreetedUsers(){
+        JDBC greet = new JDBC();
+        greet.greeter("Sino", "IsiXhosa");
+        greet.greeter("Lala", "English");
+
+        assertEquals("{Sino=1, Lala=1}", greet.greeted());
+    }
+
+    @Test
+    void shouldShowGreetedUser(){
+        JDBC greet = new JDBC();
+        greet.greeter("Sino", "English");
+        greet.greeter("Sino", "English");
+        greet.greeter("Aya", "Afrikaans");
+        greet.greeter("Aya", "IsiXhosa");
+        greet.greeter("Lala", "IsiXhosa");
+        greet.greeter("Aya", "English");
+
+        assertEquals("Aya has been greeted 3 time(s)", greet.greetedUser("Aya"));
+    }
 
 }
